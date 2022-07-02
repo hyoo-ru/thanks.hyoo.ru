bbab401d5a7bead4cc5066c52ddf1c749c2d65cb
Pavel Zubkov <zubkov_p@mail.ru>
2022-07-02 13:00:50 +0300

    +2

--- app/app.view.ts
+++ app/app.view.ts
@@ -2,6 +2,36 @@ namespace $.$$ {
 
 	export class $hyoo_thanks_app_test extends $.$hyoo_thanks_app_test {
 
+		@ $mol_mem
+		wallet() {
+			const keys = this.ton().keys()
+			const wallet = this.ton().wallet({ publicKey: keys.publicKey })
+			return wallet
+		}
+
+		wallet_create() {
+			console.log(this.wallet().ton().keys())
+			console.log(this.wallet().address().toString(true, true, true))
+			console.log(this.wallet().info())
+			console.log(this.transaction_list())
+		}
+
+		send() {
+			this.wallet().send(
+				'EQCJ8v-iq5PmU06yDlsiTFQ6n9qtIWdUamAAoG3RBKuruZKJ',
+				$mol_ton.lib().utils.toNano('0.01'),
+				'hello' + new Date().toString(),
+			)
+		}
+
+		@ $mol_mem
+		transaction_list() {
+			return this.wallet().ton().transaction_list(this.wallet().address().toString())
+		}
+
+		auto() {
+			this.wallet()
+		}
 
 	} 
 
