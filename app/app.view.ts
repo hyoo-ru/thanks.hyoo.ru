f7643d5b23054ed2f311cf704ec532e5403da5b9
jin <jin@hyoo.ru>
2022-07-02 15:50:40 +0300

    Store data

--- app/app.view.ts
+++ app/app.view.ts
@@ -15,7 +15,12 @@ namespace $.$$ {
 		
 		@ $mol_mem
 		target_name() {
-			return $mol_hash_string( this.target() ).toString(36).toUpperCase()
+			return this.short_name( this.target() )
+		}
+		
+		@ $mol_mem_key
+		short_name( name: string ) {
+			return $mol_hash_string( name ).toString(36).toUpperCase()
 		}
 		
 		@ $mol_mem
@@ -28,6 +33,37 @@ namespace $.$$ {
 		welcome_text() {
 			return super.welcome_text()
 				.replace( /{my_link}/g, '#!to=EQAyyEkW6tPWofboOqzZcHglL9kk6Az6mpnMxSwNyhDz36z7/section=target' )
+		}
+		
+		@ $mol_mem
+		likes( next?: Record< string, number > ) {
+			return this.$.$mol_state_local.value( 'likes', next ) ?? {}
+		}
+		
+		@ $mol_mem
+		subscription( next?: number ) {
+			return this.$.$mol_state_local.value( 'subscription', next ) ?? 10
+		}
+		
+		@ $mol_mem
+		target_likes( next?: number ) {
+			return this.likes(
+				next?.valueOf && { ... this.likes(), [ this.target() ]: next }
+			)[ this.target() ] ?? 0
+		}
+		
+		@ $mol_mem
+		shares() {
+			
+			const likes = this.likes()
+			const total = Object.values( likes ).reduce( ( a, b )=> a + b )
+			
+			return Object.keys( likes ).map( target => ({
+				target: this.short_name( target ),
+				likes: likes[ target ],
+				shares: `${ Math.round( likes[ target ] / total * 100 ) } %`,
+			}) )
+			
 		}
 		
 	}
