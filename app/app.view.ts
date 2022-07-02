daaa0f6d7908a39b616dca74865b32e12f40b1ce
Pavel Zubkov <zubkov_p@mail.ru>
2022-07-02 11:46:59 +0300

    send

--- app/app.view.ts
+++ app/app.view.ts
@@ -2,21 +2,35 @@ namespace $.$$ {
 
 	export class $hyoo_thanks_app_test extends $.$hyoo_thanks_app_test {
 
+		@ $mol_mem
+		wallet() {
+			const keys = this.ton().keys()
+			const wallet = this.ton().wallet({ publicKey: keys.publicKey })
+			return wallet
+		}
+
 		wallet_create() {
-			const { keys, address } = this.ton().wallet_create()
-			console.log(keys, address)
+			console.log(this.wallet().ton().keys())
+			console.log(this.wallet().address())
+			console.log(this.wallet().info())
+			console.log(this.transaction_list())
 		}
 
-		wallet_private(next?: string) {
-			return this.$.$mol_state_local.value('wallet_private', next) ?? 'none'
+		send() {
+			this.wallet().send(
+				'EQCJ8v-iq5PmU06yDlsiTFQ6n9qtIWdUamAAoG3RBKuruZKJ',
+				new $mol_ton_amount('0.01'),
+				'hello' + new Date().toString(),
+			)
 		}
 
-		wallet_public(next?: string) {
-			return this.$.$mol_state_local.value('wallet_public', next) ?? 'none'
+		@ $mol_mem
+		transaction_list() {
+			return this.wallet().ton().transaction_list(this.wallet().address().toString())
 		}
 
-		wallet_address(next?: string) {
-			return this.$.$mol_state_local.value('wallet_address', next) ?? 'none'
+		auto() {
+			this.wallet()
 		}
 
 	} 
