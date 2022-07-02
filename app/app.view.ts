de9ab709a84e601ad630165000bfa991a0f3c0bd
Pavel Zubkov <zubkov_p@mail.ru>
2022-07-02 22:01:55 +0300

    save

--- app/app.view.ts
+++ app/app.view.ts
@@ -4,25 +4,29 @@ namespace $.$$ {
 
 		@ $mol_mem
 		wallet() {
-			const keys = this.ton().keys()
-			const wallet = this.ton().wallet({ publicKey: keys.publicKey })
-			return wallet
-		}
-
-		wallet_create() {
-			console.log(this.wallet().ton().keys())
-			console.log(this.wallet().address().toString(true, true, true))
-			console.log(this.wallet().info())
-			console.log(this.transaction_list())
-		}
-
-		send() {
-			this.wallet().send()
+			return this.ton().wallet( this.keys() ) 
 		}
 
 		@ $mol_mem
-		transaction_list() {
-			return this.wallet().ton().transaction_list(this.wallet().address().toString())
+		words(next?: string[]) {
+			return this.$.$mol_store_local.value('words', next)
+		}
+
+		@ $mol_mem
+		keys() {
+			let words = this.words()
+			if (!words) words = this.words( $mol_ton_wallet.words_create() )
+			return $mol_ton_wallet.words_to_pair(words)
+		}
+
+		wallet_create() {
+			console.log(this.wallet().address().toString(true, true, true))
+			console.log(this.wallet().info())
+			console.log(this.wallet().transactions())
+		}
+
+		send() {
+			this.wallet().send('EQCJ8v-iq5PmU06yDlsiTFQ6n9qtIWdUamAAoG3RBKuruZKJ', '0.0001', 'hello world!')
 		}
 
 		auto() {
