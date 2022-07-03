7326c65e43ff1e50d5137a46a7493a4cf56f4b1f
Pavel Zubkov <zubkov_p@mail.ru>
2022-07-03 16:36:34 +0300

    Fix likes

--- app/app.view.ts
+++ app/app.view.ts
@@ -31,8 +31,9 @@ namespace $.$$ {
 		
 		@ $mol_mem
 		welcome_text() {
+			const address = this.wallet_words() ? this.wallet_address() : ''
 			return super.welcome_text()
-				.replace( /{my_link}/g, '#!to=EQAyyEkW6tPWofboOqzZcHglL9kk6Az6mpnMxSwNyhDz36z7/section=target' )
+				.replace( /{my_link}/g, `#!${address && `to=${address}`}/section=target` )
 		}
 		
 		@ $mol_mem
@@ -59,11 +60,12 @@ namespace $.$$ {
 			
 			const likes = this.likes()
 			const total = Object.values( likes ).reduce( ( a, b )=> a + b, 0 )
-			
+
 			return Object.keys( likes ).map( target => ({
 				target: this.short_name( target ),
 				likes: likes[ target ],
 				shares: `${ Math.round( likes[ target ] / total * 100 ) } %`,
+				TONs: (likes[target] / total * this.subscription()).toFixed(2),
 			}) )
 			
 		}
@@ -93,7 +95,7 @@ namespace $.$$ {
 		}
 
 		awaiting_body() {
-			if (!this.wallet_words()) return [this.Import_block()]
+			// if (!this.wallet_words()) return [this.Import_block()]
 			return [this.Subscription_block(), this.Awaiting_targets_block()]
 		}
 
@@ -104,13 +106,37 @@ namespace $.$$ {
 		}
 
 		wallet_address() {
-			return this.wallet().address().toString(true, true, true, this.wallet().ton().is_testnet())
+			return this.wallet().address().toString(true, true, true)
 		}
 
 		wallet_balance() {
 			return this.wallet().balance() + ' TON'
 		}
-		
+
+		@ $mol_action
+		enqueue_transfer_list() {
+			if (!this.wallet_words()) return
+
+			const likes = this.likes()
+			const total = Object.values( likes ).reduce( ( a, b )=> a + b, 0 )
+			if (total <= 0) return
+
+			for (const [address, count] of Object.entries(likes).reverse()) {
+				if (count <= 0) continue
+				this.enqueue_transfer( address, count / total * this.subscription() )
+			}
+		}
+
+		@ $mol_action
+		enqueue_transfer(address: string, val: number) {
+			console.log(address, val)
+			this.wallet().send(address, val.toFixed(5), `thanks.hyoo.ru`)
+		}
+
+		queue(next?: { address: string, amount: string, seqno?: number }[]) {
+			// return this.$.$mol_store_local.value()
+		}
+
 	}
 	
 }
