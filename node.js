ecc41e927ce7a5b948b2c482691a1ff3c9824878
nin-jin <nin-jin@users.noreply.github.com>
2022-10-05 07:38:27 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -9598,6 +9598,9 @@ var $;
                 return val;
             return [];
         }
+        bring() {
+            return this.Edit().bring();
+        }
         Edit() {
             const obj = new this.$.$mol_textarea_edit();
             obj.value = (val) => this.value(val);
