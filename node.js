59d744be56e5d2e0cc25d8f08f121ea85042cc6b
nin-jin <nin-jin@users.noreply.github.com>
2023-02-25 07:18:35 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -5000,6 +5000,8 @@ var $;
                 const from = el.selectionStart;
                 const to = el.selectionEnd;
                 el.value = this.value_changed(el.value);
+                if (to === null)
+                    return;
                 el.selectionEnd = to;
                 el.selectionStart = from;
                 this.selection_change(next);
@@ -5028,9 +5030,15 @@ var $;
                 el.selectionStart = from;
             }
             selection_start() {
+                const el = this.dom_node();
+                if (el.selectionStart === null)
+                    return undefined;
                 return this.selection()[0];
             }
             selection_end() {
+                const el = this.dom_node();
+                if (el.selectionEnd === null)
+                    return undefined;
                 return this.selection()[1];
             }
         }
@@ -9471,6 +9479,9 @@ var $;
         precision() {
             return 1;
         }
+        type() {
+            return "tel";
+        }
         value_string(val) {
             if (val !== undefined)
                 return val;
@@ -9487,7 +9498,7 @@ var $;
         }
         String() {
             const obj = new this.$.$mol_string();
-            obj.type = () => "tel";
+            obj.type = () => this.type();
             obj.value = (val) => this.value_string(val);
             obj.hint = () => this.hint();
             obj.enabled = () => this.string_enabled();
