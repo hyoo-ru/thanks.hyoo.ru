0fa8584b5dae9c39f14bea909a131f70df778144
nin-jin <nin-jin@users.noreply.github.com>
2024-12-18 07:16:14 +0000

    Deploying to gh-pages from @ hyoo-ru/thanks.hyoo.ru@b352636d9e4a7e4fdde148a884b1158cd74be02b 🚀

--- node.mjs
+++ node.mjs
@@ -4580,7 +4580,8 @@ var $;
                     stabilityThreshold: 100,
                 },
             });
-            watcher.on('all', (type, path) => {
+            watcher
+                .on('all', (type, path) => {
                 if (path instanceof Error) {
                     this.$.$mol_fail_log(path);
                     return;
@@ -4593,7 +4594,8 @@ var $;
                 else {
                     file.parent().reset();
                 }
-            });
+            })
+                .on('error', $mol_fail_log);
             return {
                 destructor() {
                     watcher.close();
