8ddd3de83daaea0ca271fcc12a890fdc48d6e9f7
nin-jin <nin-jin@users.noreply.github.com>
2025-01-19 07:14:23 +0000

    Deploying to gh-pages from @ hyoo-ru/thanks.hyoo.ru@b352636d9e4a7e4fdde148a884b1158cd74be02b 🚀

--- node.js
+++ node.js
@@ -6797,6 +6797,7 @@ var $;
         static go(next) {
             this.href(this.link(next));
         }
+        static commit() { }
         constructor(prefix = '') {
             super();
             this.prefix = prefix;
